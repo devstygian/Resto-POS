500e1500567ddd218039a154132cf9650528b474
devstygian <hackstygian@gmail.com>
2026-03-20 00:39:17 +0800

    Feat: Add Role-based access control function

--- src/config/config.php
+++ src/config/config.php
@@ -13,10 +13,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 function checkLogin() {
+    global $base_url;
     if (!isset($_SESSION['users']) || empty($_SESSION['users'])) {
-        header("Location: ../../../auth/login.php");
+        header("Location: {$base_url}auth/login.php");
         exit();
     }
 }
+function checkRole($roles = [])
+{
+    global $base_url;
 
+    if (!is_array($roles)) {
+        $roles = explode(',', $roles);
+    }
+
+    $roles = array_map('trim', $roles);
+
+    if (!isset($_SESSION['role']) || !in_array($_SESSION['role'], $roles, true)) {
+        header("Location: {$base_url}auth/unauthorized.php");
+        exit();
+    }
+}
 ?>
